ccc24567830c055e163e62447f5093f2bd19fa7c
Steven C <stevencch99@gmail.com>
2021-06-14 14:41:39 +0800

    Section 11: Adding a "Header" Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Fragment } from 'react';
+import Header from './components/Layout/Heading';
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header></Header>
+    </Fragment>
   );
 }
 
---
+++ src/components/Layout/Heading.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+
+import mealsImg from '../../assets/meals.jpeg';
+import classes from './Header.module.css';
+
+const Header = props => {
+  return <Fragment>
+    <header className={classes.header}>
+      <h1>ReactMeals</h1>
+      <button>Cart</button>
+    </header>
+    <div className={classes['main-image']}>
+      <img src={mealsImg} alt="A table full of delicious food
+      !" />
+    </div>
+  </Fragment>
+};
+
+export default Header;
